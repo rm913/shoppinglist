f39916a4e61a1a16344edd6f5b97e568ca78d8ea
rm913 <rachelmakovoz@gmail.com>
2015-01-07 12:31:49 -0500

    k

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
 	
+	
+	
 	$('#submit').click(function(){
 		var userInput = $("#new-item").val();
-		$('.list-items').append("<li>" + userInput + "</li>");
+		var box = $('.box').css( {
+			'display': 'inline-block',
+			'float': 'right',
+			'margin-right': '1em'
+
+		});
+		$('.list-items').append("<li>" + userInput + "</li>" + box);
 		$('#new-item').val("");
         });
-	
+
+	//$('.new-items').on('click', 'box', function() {
+	//	var check = $('.fa .fa-check-square-o').css({
+	//	'display': 'inline-block';
+	//			'float' : 'right';
+	//			'margin-right' : '1em'
+	//	});
+
+//$('check').show();
+//		$('box.').hide();
+	//});	
+
 
 
 });
