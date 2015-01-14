f64d5695a288238d4da0557d944b79ff9af97b53
rm913 <rachelmakovoz@gmail.com>
2015-01-14 08:45:56 -0500

    j

--- js/app.js
+++ js/app.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
 
-	var num = prompt('Choose a number');
-	if (num % 5 == 0) {
-		console.log('weve got a fizzy!');
-	}
-	else if (num % 9 == 0){
-		console.log('weve got a buzzy');
-	}
-	else {
-		console.log(num);
-	};
+	var num = $('#number').val();
+
+	$('#submit').click(function(){
+
 	
 
+	if (num % 3 == 0) {
+		
+		$('.answer ul').append('<li>'+ "Fizz" + '</li>');
+	}
+	else if (num % 5 == 0) {
+		$('.answer ul').append('<li>'+ "Buzz" + '</li>');
+	}
+	else if (num % 5 == 0 && num % 3 ==0) {
+		$('.answer ul').append('<li>'+ "Fizz-Buzz" + '</li>');
+	}
+	else {
+		('.answer ul').append('<li>'+ num + '</li>');
+	}
+	
+});
 
 });
