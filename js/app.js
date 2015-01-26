31221827ca3cbad20a602bb7c734dd13810d4467
rm913 <rachelmakovoz@gmail.com>
2015-01-26 12:42:28 -0500

    k

--- js/app.js
+++ js/app.js
@@ -6,18 +6,24 @@ $(document).ready(function() {
 	$('#submit').click(function(){
 		var userInput = $("#new-item").val();
 		
-		$('.list-items').append("<li>" + userInput+"<i class='fa fa-square-o'></i>" +"<i class='fa-trash'></i>"+"</li>");
-	//	$('.fa-square-o').css({
-			
-	//		'font-size': '1.5em'
-			
-	//});
+		 $('.list-items').append("<li>" + userInput+
+		 	"<i class='fa fa-square-o square'></i>" +
+		 	"<i class='fa fa-trash fa-4'></i>"+"</li>");
 		$('#new-item').val("");
         });
 
-	$('.list-items').on("click", ".fa-trash", function(){
-		$('this').closest('li').remove();
+	$(".list-items").on("click", ".fa-trash", function(){
+		$(this).closest('li').remove();
+		console.log('ths has been deleted');
 });
+	$(".list-items").on("click", ".fa-square-o",function(){
+		$(this).toggleClass("fa-check-square-o fa-square-o");
+		
+	});
+	$(".list-items").on("click", ".fa-check-square-o",function(){
+		$(this).toggleClass("fa-check-square-o fa-square-o");
+		
+	});
 
 
 });
